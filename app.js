9c929cf6155a7675edf3a164bc4b85cf80376928
xJaimeGx <beanieman019@protonmail.com>
2022-02-16 16:21:43 -0600

    take console log out

---
+++ app.js
@@ -0,0 +1,17 @@
+const profileDataArgs = process.argv.slice(2);
+
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+  //This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // is the same as this.
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
