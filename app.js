32280f848e9b619a11e515e9fc3056c373f33313
xJaimeGx <beanieman019@protonmail.com>
2022-02-20 11:38:50 -0600

    modularize html

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const profileDataArgs = process.argv.slice(2);
-
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// This...
+const [name, github] = profileDataArgs;
+// is the same as these...
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
 
 // Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-  //This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+//const printProfileData = profileDataArr => {
+  // This...
+  //for (let i = 0; i < profileDataArr.length; i += 1) {
+    //console.log(profileDataArr[i]);
+  //}
 
-  console.log('================');
+  //console.log('================');
 
   // is the same as this.
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+  //profileDataArr.forEach(profileItem => console.log(profileItem));
+//};
 
-printProfileData(profileDataArgs);
+//printProfileData(profileDataArgs);
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
